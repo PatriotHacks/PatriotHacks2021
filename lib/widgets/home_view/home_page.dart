45ba6980ddef0d63c1036f03e4106f1e529cce95
Caleb Owens <caleb.ja.owens@gmail.com>
2020-12-07 13:06:54 -0500

    Dealt with padding on mobile version of home page

--- lib/widgets/home_view/home_page.dart
+++ lib/widgets/home_view/home_page.dart
@@ -27,7 +27,7 @@ class HomePage extends StatelessWidget {
     return ResponsiveBuilder(
       builder: (context, sizingInformation) {
         return Padding(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.all(40),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
