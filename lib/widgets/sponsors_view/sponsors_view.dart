f68cc8635324f68caf46861f8dca24638f641b73
Chandraaditya Putrevu <chandraaditya2001@gmail.com>
2020-12-08 22:39:33 +0530

    more bug fixes

--- lib/widgets/sponsors_view/sponsors_view.dart
+++ lib/widgets/sponsors_view/sponsors_view.dart
@@ -1,7 +1,6 @@
 import 'package:patriot_hacks/widgets/body/body.dart';
 import 'package:flutter/material.dart';
 import 'package:patriot_hacks/widgets/sponsors_view/SponsorsDesktop.dart';
-import 'package:patriot_hacks/widgets/sponsors_view/Sponsors_mobile.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class SponsorsView extends StatelessWidget {
