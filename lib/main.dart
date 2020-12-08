2d1dedb03eec88ed3513cdbd2ea2e678bef8cdc2
Chandraaditya Putrevu <chandraaditya2001@gmail.com>
2020-12-08 22:51:41 +0530

    more bug fixes 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'configure_nonweb.dart' if (dart.library.html) 'configure_web.dart';
 
 
 void main() {
-    //NOTE: This needs to be enabled in flutter master channel for the # to disappear in the URL. It stopped working on the beta channel so we either have to wait or switch to master channel
+  //NOTE: This needs to be enabled in flutter master channel for the # to disappear in the URL. It stopped working on the beta channel so we either have to wait or switch to master channel
   configureApp();
   setupLocator();
   runApp(Main());
@@ -20,10 +20,7 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (context, child) {
-        print(child.runtimeType);
-        return LayoutTemplate(child: child);
-      },
+      builder: (context, child) => LayoutTemplate(child: child),
       navigatorKey: locator<NavigationService>().navigatorKey,
       onGenerateRoute: generateRoute,
       initialRoute: HomeRoute,
